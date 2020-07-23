51561b61b9cf3685565a2753fe68da30a0b8fae7
HanSangHyeok <tkdgur8377@gmail.com>
2020-07-23 16:17:11 +0900

    step2. if문

---
+++ baekjoon/step2/num2884.java
@@ -0,0 +1,20 @@
+package package2;
+
+import java.util.Scanner;
+
+public class num2884 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		
+		if(num2>=45) {
+			num2 -=45;
+			System.out.println(num1 + " " + num2);
+		}else {
+			num1 -=1;
+			num2 +=15; 
+			System.out.println(num1 + " " + num2);
+		}
+	}
+}
