3d1384e16341112f75479409523cabaad8dc132f
HanSangHyeok <tkdgur8377@gmail.com>
2020-08-28 23:48:03 +0900

    step8. 문자열

--- baekjoon/step8/num1152.java
+++ baekjoon/step8/num1152.java
@@ -1,5 +1,32 @@
 package package8;
 
-public class num1152 {
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
+public class num1152 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		String[] array = str.split(" ");
+		String[] temp = new String[2];
+		StringBuilder builder = new StringBuilder();
+		int[] value = new int[2];
+		
+		for(int j=0;j<2;j++) {
+			for(int i=3;i>0;i--) {
+				builder.append(array[j].charAt(i-1));
+			}
+			str = builder.toString();
+			value[j] = Integer.parseInt(str);
+			builder.delete(0,builder.length());
+		}
+		
+		if(value[0]>value[1]) {
+			System.out.println(value[0]);
+		}else {
+			System.out.println(value[1]);
+		}
+		
+		
+	}
 }
--- baekjoon/step8/num5622.java
+++ baekjoon/step8/num5622.java
@@ -1,5 +1,33 @@
 package package8;
 
-public class num5622 {
+import java.util.Scanner;
 
+public class num5622 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String data = sc.nextLine();
+		int dataLen = data.length();
+		int index =0;
+		int value =0;
+		int total = 0;
+		for(int i=0; i<dataLen; i++) {
+			index=data.charAt(i) - 'A';
+			if(index >17) {
+				if(index == 18) {
+					value = 5;
+				}else {
+					if(index>21) {
+						value=7;
+					}else {
+						value=6;
+					}
+				}
+			}else {
+				value=index/3;
+			}
+			total+=(value+3);
+		}
+		System.out.println(total);
+	}
 }
