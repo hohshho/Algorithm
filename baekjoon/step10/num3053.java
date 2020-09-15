cbaddc92f36d196bb62ddc8a044172c562ff8664
HanSangHyeok <tkdgur8377@gmail.com>
2020-09-15 23:43:13 +0900

    step10. finish

--- baekjoon/step10/num3053.java
+++ baekjoon/step10/num3053.java
@@ -1,5 +1,17 @@
 package package10;
 
-public class num3053 {
+import java.util.Scanner;
 
+public class num3053 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int R=sc.nextInt();
+		
+		String res1 = String.format("%.6f", Math.PI*R*R);
+		String res2 = String.format("%.6f", (double)2*R*R);
+		
+		System.out.println(res1);
+		System.out.println(res2);
+	}
 }
--- baekjoon/step9/num2775.java
+++ baekjoon/step9/num2775.java
@@ -1,8 +1,46 @@
 package package9;
 
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
 public class num2775 {
-    public static void main(String[] args){
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-    }
+ 
+	public static int[][] APT = new int[15][15];
+ 
+	public static void main(String[] args) throws IOException {
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		make_APT();	
+		
+		int T = Integer.parseInt(br.readLine());
+ 
+		for (int i = 0; i < T; i++) {
+			int n = Integer.parseInt(br.readLine());
+			int k = Integer.parseInt(br.readLine());
+			sb.append(APT[n][k]).append('\n');
+		}
+		System.out.println(sb);
+	}
+ 
+	
+	public static void make_APT() {
+		// 아파트 생성
+ 
+		for (int i = 0; i < 15; i++) {
+			APT[i][1] = 1; 
+			APT[0][i] = i; 
+		}
+ 
+		for (int i = 1; i < 15; i++) { 
+ 
+			for (int j = 2; j < 15; j++) { 
+				APT[i][j] = APT[i][j - 1] + APT[i - 1][j];
+			}
+		}
+	}
+ 
 }
