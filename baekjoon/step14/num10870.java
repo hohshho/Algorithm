9a5729bb6b323205fdbd77b5869c12b7d5bc6534
HanSangHyeok <tkdgur8377@gmail.com>
2020-10-09 18:24:23 +0900

    step14

---
+++ baekjoon/step14/num10870.java
@@ -0,0 +1,7 @@
+package package14;
+
+public class num10870 {
+	public static void main(String[] args) {
+		
+	}
+}
--- baekjoon/step14/num1463.java
+++ baekjoon/step14/num1463.java
@@ -1,33 +1,26 @@
 package package14;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
-public class num2579 {
+public class num1463 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int N = Integer.parseInt(br.readLine());
-		int[] value = new int[N+1];
 		int[] dp = new int[N+1];
 		
-		for(int i=0; i<N; i++) {
-			value[i+1] = Integer.parseInt(br.readLine());
-		}
+		dp[1] = 0;
 		
-		if(N==0) {
-			System.out.println(0);
-		}else if(N<2){
-			System.out.println(value[N]);
-		}else {
-		
-			for(int i=N; i>0;i--) {
-			
-			}
+		for(int i=2;i<=N;i++) {
+			dp[i] = dp[i-1]+1;
+			if(i%2==0)
+				dp[i] = Math.min(dp[i], dp[i/2]+1);
+			if(i%3==0)
+				dp[i] = Math.min(dp[i], dp[i/3]+1);
 		}
+		System.out.println(dp[N]);
 		
 	}
 }
