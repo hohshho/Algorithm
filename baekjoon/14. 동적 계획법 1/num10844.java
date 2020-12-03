41ddf0ff5e9bd1f5b7dcf77082aa71db1ec94893
HanSangHyeok <tkdgur8377@gmail.com>
2020-12-03 23:25:20 +0900

    dp, math 문제 풀이

--- baekjoon/14. 동적 계획법 1/num10844.java
+++ baekjoon/14. 동적 계획법 1/num10844.java
@@ -7,13 +7,13 @@ import java.io.InputStreamReader;
 public class num10844 {
 
 	static int[] dp;
+	static int dpLen = 0;
 	static int count =0;
 	static void calc(int num, int depth) {
-		if(depth==dp.length-1) {
+		if(depth==dpLen) {
 			count++;
 			return;
 		}
-		dp[depth] = num;
 		if(num+1<10) {
 			calc(num+1,depth+1);
 		}
@@ -25,11 +25,10 @@ public class num10844 {
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		
 		int N = Integer.parseInt(br.readLine());
 		dp = new int[N+1];
-		
+		dpLen = dp.length-1;
 		for(int i=1;i<10;i++) {
 			calc(i,1);
 		}
---
+++ baekjoon/16. 수학3/num1037.java
@@ -0,0 +1,34 @@
+package package16;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class num1037 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int max = 0;
+		int min = 0;
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		String[] data = br.readLine().split(" ");
+		int[] arr = new int[n+1];
+		for(int i=0;i<n;i++) {
+			arr[i] = Integer.parseInt(data[i]);
+			if(i==0) {
+				max = arr[i];
+				min = arr[i];
+			}
+			max = arr[i]>max ? arr[i] : max;
+			min = arr[i]<min ? arr[i] : min;
+		}
+		
+		
+		System.out.println(max*min);
+		
+	}
+
+}
