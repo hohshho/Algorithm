191e5af56c0ecb7f0caa92de28add454cd215687
HanSangHyeok <tkdgur8377@gmail.com>
2021-01-12 23:51:20 +0900

    조합 풀이

--- baekjoon/16. 정수론,조합론/num11050.java
+++ baekjoon/16. 정수론,조합론/num11050.java
@@ -1,10 +1,26 @@
 package package16;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class num11050 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] inputData = br.readLine().split(" ");
+		int N = Integer.parseInt(inputData[0]);
+		int K = Integer.parseInt(inputData[1]);
+		
+		System.out.println(combination(N, K));
+		
+		
+		
 	}
-
+	public static int combination(int n, int r) {
+		if(n == r || r == 0) 
+			return 1; 
+		else 
+			return combination(n - 1, r - 1) + combination(n - 1, r); 
+	}
 }
--- baekjoon/16. 정수론,조합론/num11051.java
+++ baekjoon/16. 정수론,조합론/num11051.java
@@ -1,10 +1,27 @@
 package package16;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class num11051 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static int[][] dp;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] inputData = br.readLine().split(" ");
+		int N = Integer.parseInt(inputData[0]);
+		int K = Integer.parseInt(inputData[1]);
+		dp = new int[N+1][K+1];
+		
+		System.out.println(combination(N, K));
+		
 	}
-
+	public static int combination(int n, int r) {
+		if(dp[n][r] > 0)
+			return dp[n][r];
+		if(n == r || r == 0) 
+			return dp[n][r] = 1;
+		else 
+			return dp[n][r] = (combination(n - 1, r - 1) + combination(n - 1, r))%10007; 
+	}
 }
