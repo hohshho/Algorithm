b92af53daa64fdf49fd77d6e5dc9ecf02b1c06a9
HanSangHyeok <tkdgur8377@gmail.com>
2021-01-13 12:19:54 +0900

    정수론, 조합론 풀이 완료

--- baekjoon/16. 정수론,조합론/num1010.java
+++ baekjoon/16. 정수론,조합론/num1010.java
@@ -1,10 +1,33 @@
 package package16;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class num1010 {
+	static int[][] dp;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		for(int i=0;i<T;i++) {
+			String[] data = br.readLine().split(" ");
+			int n = Integer.parseInt(data[0]);
+			int k = Integer.parseInt(data[1]);
+			dp = new int[k+1][n+1];
+			sb.append(combination(k,n)+"\n");
+		}
+		System.out.println(sb);
+		
 	}
-
+	public static int combination(int n, int k) {
+		if(dp[n][k] > 0 ) {
+			return dp[n][k];
+		}
+		if(n == k || k == 0)
+			return dp[n][k] = 1;
+		else
+			return dp[n][k] = combination(n-1,k-1) + combination(n-1,k);
+	}
 }
