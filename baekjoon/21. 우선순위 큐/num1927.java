33a65cd93732a2c7eff9b0d4a60cc5dff3fe78f2
HanSangHyeok <tkdgur8377@gmail.com>
2021-01-14 12:31:32 +0900

    우선순위 큐 완료

---
+++ baekjoon/21. 우선순위 큐/num1927.java
@@ -0,0 +1,33 @@
+package package21;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class num1927 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		PriorityQueue<Integer> q = new PriorityQueue<>();
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int i=0; i<T; i++) {
+			int value = Integer.parseInt(br.readLine());
+			if(value!=0)
+				q.offer(value);
+			else {
+				if(!q.isEmpty()) {
+					sb.append(q.poll());
+				}else {
+					sb.append("0");
+				}
+				sb.append("\n");
+			}
+		}
+		System.out.println(sb);
+	}
+
+}
