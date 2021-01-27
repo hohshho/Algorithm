57e55e0661b7f5117303aa6c94db757dec8da496
HanSangHyeok <tkdgur8377@gmail.com>
2021-01-27 18:17:07 +0900

    벨만포드 경로 출력 변경

---
+++ baekjoon/유형별문제/트리/num2250.java
@@ -0,0 +1,22 @@
+package tree;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class num2250 {
+
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		
+	}
+	
+	static class Node{
+		int top;
+		Node(int top) {
+			this.top = top;
+		}
+	}
+
+}
