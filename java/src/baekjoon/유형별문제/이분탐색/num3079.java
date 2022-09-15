ff876d742e82735415c13d3256f09d9d070347ba
hohshho <tkdgur8377@gmail.com>
2022-09-15 17:27:07 +0900

    이분탐색 마무리

--- java/src/baekjoon/유형별문제/이분탐색/num3079.java
+++ java/src/baekjoon/유형별문제/이분탐색/num3079.java
@@ -1,4 +1,62 @@
 package binarySearch;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class num3079 {
+    static long N, M, line[], max = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String[] input = br.readLine().split(" ");
+
+        N = stol(input[0]);
+        M = stol(input[1]);
+
+        line = new long[(int) N];
+
+        for(int i=0; i<N; i++){
+            line[i] = stol(br.readLine());
+            max = line[i] > max ? line[i] : max;
+        }
+
+        max *=M;
+
+        System.out.println(bsearch());
+
+    }
+
+    public static long bsearch(){
+        long result = 0;
+        long start = 0, end = max, mid;
+
+        while(end >= start){
+            mid = (start + end) / 2;
+
+            if(checkTime(mid)){
+                result = mid;
+                end = mid - 1;
+            }else {
+                start = mid + 1;
+            }
+        }
+
+        return result;
+    }
+
+    public static boolean checkTime(long target){
+        long cnt = 0;
+        for(int i=0; i<N; i++){
+            cnt +=  target / line[i];
+        }
+
+        return cnt >= M ? true : false;
+    }
+
+    public static long stol(String s){
+        return Long.parseLong(s);
+    }
 }
--- java/src/baekjoon/유형별문제/이분탐색/num7795.java
+++ java/src/baekjoon/유형별문제/이분탐색/num7795.java
@@ -1,4 +1,72 @@
 package binarySearch;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class num7795 {
+    static int T, A[], B[], N, M, result[];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        T = stoi(br.readLine());
+        result = new int[T];
+
+        for (int i = 0; i < T; i++) {
+            String[] firstLine = br.readLine().split(" ");
+            String[] aLine = br.readLine().split(" ");
+            String[] bLine = br.readLine().split(" ");
+
+            N = stoi(firstLine[0]);
+            M = stoi(firstLine[1]);
+            A = new int[N + 1];
+            B = new int[M + 1];
+
+            for (int j = 1; j <= N; j++) {
+                A[j] = stoi(aLine[j - 1]);
+            }
+
+            for (int j = 1; j <= M; j++) {
+                B[j] = stoi(bLine[j - 1]);
+            }
+
+            Arrays.sort(B);
+
+            sb.append(Bsearch() + "\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+
+    public static int Bsearch() {
+        int start = 0, end = M, cnt = 0, mid = (start + end) / 2, ret = 0;
+
+        for (int j = 1; j <= N; j++) {
+            while (end >= start) {
+                mid = (start + end) / 2;
+
+                if (A[j] > B[mid]) {
+                    ret = mid;
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+
+            start = 1;
+            end = M;
+            cnt += ret;
+            ret = 0;
+        }
+
+        return cnt;
+    }
+
+    public static int stoi(String s) {
+        return Integer.parseInt(s);
+    }
 }
