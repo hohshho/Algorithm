06558ac41776cd289278bd45e43e89a33d7b473d
hohshho <tkdgur8377@gmail.com>
2022-11-07 20:52:45 +0900

    백트래킹

---
+++ java/src/baekjoon/유형별문제/스택/num6549.java
@@ -0,0 +1,16 @@
+package stack;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class num6549 {
+    public static void main(String[] args){
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+
+    public static int stoi(String s){
+        return Integer.parseInt(s);
+    }
+}
