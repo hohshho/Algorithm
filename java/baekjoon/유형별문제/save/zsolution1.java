35125445623336c3dc176758594f6ceec49668bf
hohshho <tkdgur8377@gmail.com>
2022-11-12 23:51:44 +0900

    save

---
+++ java/baekjoon/유형별문제/save/zsolution1.java
@@ -0,0 +1,16 @@
+package save;
+
+
+
+public class zsolution1 {
+    static class solution {
+        // 백준 1834
+        public long solution(long n) {
+            return n * (n - 1) * (n + 1) / 2;
+        }
+    }
+
+    public static void main(String[] args){
+        solution a = new solution();
+    }
+}
