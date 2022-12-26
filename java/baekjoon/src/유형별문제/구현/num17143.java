6cbc6b9bc2dc08c49666fdd3a8439f12ddf19b91
hohshho <tkdgur8377@gmail.com>
2022-12-26 23:48:55 +0900

    4828, 17143 17143 수정 필요 (map 없애고 2차원 배열로 풀어야 할 듯 )

--- java/baekjoon/src/유형별문제/구현/num17143.java
+++ java/baekjoon/src/유형별문제/구현/num17143.java
@@ -11,8 +11,8 @@ import java.util.Iterator;
 public class num17143 {
     // FIXME: 풀던 문제
     static int R, C, M, result;
-    // 위, 아래, 오른쪽, 왼쪽
-    static int[] dx = {0, 0, 1, -1}, dy = {1, -1, 0, 0};
+    // 위, 좌, 아래, 오른쪽
+    static int[] dx = {0, -1, 0, 1}, dy = {-1, 0, 1, 0};
     static HashMap<Integer, Shark> sharks = new HashMap<Integer, Shark>();
     // 열 번호, 열에 속한 상어
     static HashMap<Integer, ArrayList<Integer>> map = new HashMap<Integer, ArrayList<Integer>>();
@@ -26,63 +26,76 @@ public class num17143 {
         C = stoi(firstLine[1]);
         M = stoi(firstLine[2]);
 
-        for (int i = 0; i < M; i++) {
+        for (int i = 1; i <= C; i++) {
+            ArrayList<Integer> list = new ArrayList<Integer>();
+            map.put(i, list);
+        }
+
+        for (int i = 1; i <= M; i++) {
             String[] sharkData = br.readLine().split(" ");
 
-            int x = stoi(sharkData[0]) - 1;
-            int y = stoi(sharkData[1]) - 1;
+            int y = stoi(sharkData[0]);
+            int x = stoi(sharkData[1]);
             int speed = stoi(sharkData[2]);
             int direction = stoi(sharkData[3]) - 1;
             int size = stoi(sharkData[4]);
 
+            // 상, 좌, 하, 우
+            if (direction == 1)
+                direction = 2;
+            else if (direction == 2)
+                direction = 3;
+            else if (direction == 3)
+                direction = 1;
+
             sharks.put(i, new Shark(x, y, speed, direction, size));
 
-            ArrayList<Integer> list;
-            if(map.containsKey(y)){
-                list = map.get(y);
-                map.remove(y);
-            }else {
-                list = new ArrayList<Integer>();
-            }
+            ArrayList<Integer> list = map.get(x);
 
             list.add(i);
-            map.put(y, list);
+            // key : x좌표 / value : 상어 index
+            map.put(x, list);
         }
 
-        for (int i = 0; i < C; i++) {
+        for (int i = 1; i <= C; i++) {
             // 1. 상어 잡기
-            if(map.containsKey(i)){
-                int x = 101; // R, C 100이하
+            if (map.containsKey(i)) {
+                int y = 101; // R, C 100이하
                 int getShark = 0;
                 ArrayList<Integer> list = map.get(i);
-                if(list == null){
-                    continue;
-                }
-                for(int SharkNum : list){
-                    // 가장 가까운 상어 index
-                    Shark item = sharks.get(SharkNum);
-                    if(x > item.x){
-                        x = item.x;
-                        getShark = SharkNum;
+
+                if (!list.isEmpty()) {
+                    for (int SharkNum : list) {
+                        // 가장 가까운 상어 index
+                        Shark item = sharks.get(SharkNum);
+                        if (y > item.y) {
+                            y = item.y;
+                            getShark = SharkNum;
+                        }
                     }
-                }
 
-                Shark getSharkObj = sharks.get(getShark);
-                result += getSharkObj.size;
+                    Shark getSharkObj = sharks.get(getShark);
+                    result += getSharkObj.size;
 
-                sharks.remove(getShark);
-                list.remove(getShark);
+                    sharks.remove(getShark);
+                    list.remove(Integer.valueOf(getShark));
 
-                map.remove(i);
-                if(list.size() != 0){
-                    map.put(i, list);
+                    map.remove(i);
+                    if (list.size() != 0) {
+                        map.put(i, list);
+                    }
                 }
             }
 
             // map 초기화
+            map.clear();
+            for (int j = 1; j <= C; j++) {
+                ArrayList<Integer> list = new ArrayList<Integer>();
+                map.put(j, list);
+            }
 
             // 2. 상어 이동
-            for(int key : sharks.keySet()){
+            for (int key : sharks.keySet()) {
                 Shark item = sharks.get(key);
                 int x = item.x;
                 int y = item.y;
@@ -90,83 +103,53 @@ public class num17143 {
                 int direction = item.direction;
                 int size = item.size;
 
-                if(item.direction == 0 && item.direction == 1) {
-                    int rotate = (R-1) * 2;
-                    if(item.speed >= rotate) {
-                        speed = speed % rotate;
+                // 속력만큼 상어 이동 시키기
+                if (direction == 0 || direction == 2) // 상 하
+                    speed %= (R - 1) * 2;
+                else if (direction == 1 || direction == 3) // 좌 우
+                    speed %= (C - 1) * 2;
+
+                for (int s = 0; s < speed; s++) {
+                    // 현재 r, c에 방향에 맞게 1칸씩 추가하며 위치 이동
+                    int ny = y + dy[direction];
+                    int nx = x + dx[direction];
+
+                    // 이동할 새로운 위치가 범위를 벗어나 벽에 부딪히면
+                    if (ny < 1 || ny > R || nx < 1 || nx > C) {
+                        y -= dy[direction]; // 다시 값 돌려주고
+                        x -= dx[direction];
+                        direction = (direction + 2) % 4; // 방향 반대로
+                        continue;
                     }
 
-                    for(int j = 0; j < speed; j++){
-                        int nx = x + dx[direction];
-                        int ny = y + dy[direction];
-
-                        // 위 벽에 닿을 경우
-                        if(nx < 1) {
-                            direction = 2;
-                            nx = nx + 2;
-                        }
-
-                        // 아래 벽에 닿을 경우
-                        if(nx > R){
-                            direction = 1;
-                            nx = nx - 2;
-                        }
-                        x = nx;
-                        y = ny;
-                    }
-
-                }else {
-                    int rotate = (C-1) * 2;
-                    if(item.speed >= rotate) {
-                        speed = speed % rotate;
-                    }
-
-                    for(int j = 0; j < speed; j++){
-                        int nx = x + dx[direction];
-                        int ny = y + dy[direction];
-
-                        // 왼쪽 벽에 닿을 경우
-                        if(ny < 1) {
-                            direction = 3;
-                            ny = ny + 2;
-                        }
-
-                        // 오른쪽 벽에 닿을 경우
-                        if(ny > C){
-                            direction = 4;
-                            ny = ny - 2;
-                        }
-                        x = nx;
-                        y = ny;
-                    }
+                    // 위치 벗어나지 않을때는 새로운 위치로 이동
+                    y = ny;
+                    x = nx;
                 }
 
                 sharks.put(key, new Shark(x, y, speed, direction, size));
 
-                ArrayList list;
-                if(map.containsKey(y)){
-                    list = map.get(y);
-                    map.remove(y);
-                }else {
-                    list = new ArrayList<>();
-                }
+                ArrayList<Integer> list = map.get(x);
 
                 list.add(key);
-                map.put(y, list);
+                // key : x좌표 / value : 상어 index
+                map.put(x, list);
             }
 
             // 3. 동일한 위치 상어 체크
-            for(int key : map.keySet()){
+            for(int key : map.keySet()) {
                 ArrayList<Integer> list = map.get(key);
                 ArrayList<Integer> deleteItems = new ArrayList<>();
 
-                for(int j=0; j<list.size(); j++){
+                if (list.size() < 2) continue;
+
+                for (int j = 0; j < list.size(); j++) {
                     Shark firstItem = sharks.get(list.get(j));
 
-                    for(int k=j; k < list.size(); k++){
+                    for (int k = j + 1; k < list.size(); k++) {
                         Shark secondItem = sharks.get(list.get(k));
 
-                        if(firstItem.x == secondItem.x) {
+                        if (firstItem.y == secondItem.y) {
                             deleteItems.add(firstItem.size > secondItem.size ? k : j);
                         }
                     }
--- java/baekjoon/src/유형별문제/구현/num4828.java
+++ java/baekjoon/src/유형별문제/구현/num4828.java
@@ -2,16 +2,123 @@ package 유형별문제.구현;
 
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class num4828 {
-    public static void main(String[] args) {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static String inputLine;
 
+    public static void main(String[] args) throws IOException {
+        while(inputData()){
+            XMLchecker checker = new XMLchecker(inputLine);
 
-//        1. escape 문자 제거 (&lt; &gt; &amp;)
-//        2. 16진수 검증 & 제거
-//        3. xml 태그 검증 & 제거
-//        4. 위 3가지가 제거된 plain text의 검증
+            System.out.println(checker.run(inputLine) ? "valid" : "invalid");
+        }
+    }
+
+    static boolean inputData() throws IOException {
+        inputLine = br.readLine();
+
+        if(inputLine == null) return false;
+
+        return true;
+    }
+
+    static class XMLchecker{
+        String line;
+
+        XMLchecker(String line){
+            this.line = line;
+        }
+
+        boolean run(String line) {
+            // 1. escape 문자 제거 (&lt; &gt; &amp;)
+            deleteExceptionCharacter();
+
+            // 2. 16진수 검증 & 제거
+            if(!verificateAndDeleteHexadecimal()) return false;
+
+            // 3. xml 태그 검증 & 제거
+            if(!verificateAndDeleteXML()) return false;
+
+            // 4. 위 3가지가 제거된 plain text의 검증
+            if(!verificatePlainText()) return false;
+
+            return true;
+        }
+
+        boolean verificatePlainText(){
+            if (this.line.contains("<") || this.line.contains(">") || this.line.contains("&")) {
+                return false;
+            }
+
+            for (char ch : this.line.toCharArray()) {
+                if (ch < 32 || 127 < ch) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        boolean verificateAndDeleteXML(){
+            Stack<String> ctx = new Stack<>();
+
+            this.line = this.line.replaceAll("<[a-z0-9]+/>", "0"); // test case : <a><<a/>/a> /
+            Matcher m = Pattern.compile("</?[a-z0-9]+>").matcher(this.line);
+
+            while (m.find()) {
+                String rawTag = m.group();
+
+                if (rawTag.startsWith("</")) {
+                    if(ctx.empty()) return false; // empty stack check
+
+                    String openTag = ctx.pop().replaceAll("[<>/]", "");
+                    String closeTag = rawTag.replaceAll("[<>/]", "");
+
+                    if (openTag.equals(closeTag) == false) {
+                        return false;
+                    }
+                } else {
+                    ctx.push(rawTag);
+                }
+            }
+
+            if (ctx.isEmpty() == false) {
+                return false;
+            }
+
+            this.line = this.line.replaceAll("</?[a-z0-9]+>", "");
+
+            return true;
+        }
+
+        boolean verificateAndDeleteHexadecimal(){
+            Matcher m = Pattern.compile("&x[a-fA-F0-9]+;").matcher(this.line);
+
+            while (m.find()) {
+                String hex = m.group().replaceAll("[&x;]", "");
+
+                if (hex.length() % 2 != 0) {
+                    return false;
+                }
+            }
+
+            this.line = this.line.replaceAll("&x[a-fA-F0-9]+;", "");
+
+            return true;
+        }
+
+        void deleteExceptionCharacter(){
+            this.line = this.line.replaceAll("(&amp;)|(&lt;)|(&gt;)", "");
+        }
+    }
+
+    static int stoi(String s){
+        return Integer.parseInt(s);
     }
 }
