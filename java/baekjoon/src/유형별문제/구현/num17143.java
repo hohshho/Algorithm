1635679460127b74365dc5f77e9203168e0785e5
hohshho <tkdgur8377@gmail.com>
2023-01-14 15:44:19 +0900

    구현

--- java/baekjoon/src/유형별문제/구현/num17143.java
+++ java/baekjoon/src/유형별문제/구현/num17143.java
@@ -1,186 +1,131 @@
 package 유형별문제.구현;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Iterator;
+import java.io.*;
+import java.util.*;
 
 public class num17143 {
-    // FIXME: 풀던 문제
-    static int R, C, M, result;
-    // 위, 좌, 아래, 오른쪽
-    static int[] dx = {0, -1, 0, 1}, dy = {-1, 0, 1, 0};
-    static HashMap<Integer, Shark> sharks = new HashMap<Integer, Shark>();
-    // 열 번호, 열에 속한 상어
-    static HashMap<Integer, ArrayList<Integer>> map = new HashMap<Integer, ArrayList<Integer>>();
+    static int R, C, M;
+    static int answer = 0;
+    static int[][] map;
+    static int[][] deltas = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+    static StringTokenizer st;
+    static List<Shark> list = new ArrayList<>();
+
+    static class Shark {
+        int r, c, s, d, z;
+
+        public Shark(int z) {
+            super();
+            this.z = z;
+        }
+
+        public Shark(int r, int c, int s, int d, int z) {
+            super();
+            this.r = r;
+            this.c = c;
+            this.s = s;
+            this.d = d;
+            this.z = z;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            Shark other = (Shark) obj;
+            if (z != other.z)
+                return false;
+            return true;
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        String[] firstLine = br.readLine().split(" ");
-        R = stoi(firstLine[0]);
-        C = stoi(firstLine[1]);
-        M = stoi(firstLine[2]);
+        st = new StringTokenizer(br.readLine());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[R + 1][C + 1];
 
-        for (int i = 1; i <= C; i++) {
-            ArrayList<Integer> list = new ArrayList<Integer>();
-            map.put(i, list);
-        }
-
-        for (int i = 1; i <= M; i++) {
-            String[] sharkData = br.readLine().split(" ");
-
-            int y = stoi(sharkData[0]);
-            int x = stoi(sharkData[1]);
-            int speed = stoi(sharkData[2]);
-            int direction = stoi(sharkData[3]) - 1;
-            int size = stoi(sharkData[4]);
-
-            // 상, 좌, 하, 우
-            if (direction == 1)
-                direction = 2;
-            else if (direction == 2)
-                direction = 3;
-            else if (direction == 3)
-                direction = 1;
-
-            sharks.put(i, new Shark(x, y, speed, direction, size));
-
-            ArrayList<Integer> list = map.get(x);
-
-            list.add(i);
-            // key : x좌표 / value : 상어 index
-            map.put(x, list);
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int r = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            int s = Integer.parseInt(st.nextToken());
+            int d = Integer.parseInt(st.nextToken()) - 1;
+            int z = Integer.parseInt(st.nextToken());
+            map[r][c] = z;
+            list.add(new Shark(r, c, s, d, z));
         }
 
         for (int i = 1; i <= C; i++) {
-            // 1. 상어 잡기
-            if (map.containsKey(i)) {
-                int y = 101; // R, C 100이하
-                int getShark = 0;
-                ArrayList<Integer> list = map.get(i);
-
-                if (!list.isEmpty()) {
-                    for (int SharkNum : list) {
-                        // 가장 가까운 상어 index
-                        Shark item = sharks.get(SharkNum);
-                        if (y > item.y) {
-                            y = item.y;
-                            getShark = SharkNum;
-                        }
-                    }
-
-                    Shark getSharkObj = sharks.get(getShark);
-                    result += getSharkObj.size;
-
-                    sharks.remove(getShark);
-                    list.remove(Integer.valueOf(getShark));
-
-                    map.remove(i);
-                    if (list.size() != 0) {
-                        map.put(i, list);
-                    }
-                }
-            }
-
-            // map 초기화
-            map.clear();
-            for (int j = 1; j <= C; j++) {
-                ArrayList<Integer> list = new ArrayList<Integer>();
-                map.put(j, list);
-            }
-
-            // 2. 상어 이동
-            for (int key : sharks.keySet()) {
-                Shark item = sharks.get(key);
-                int x = item.x;
-                int y = item.y;
-                int speed = item.speed;
-                int direction = item.direction;
-                int size = item.size;
-
-                // 속력만큼 상어 이동 시키기
-                if (direction == 0 || direction == 2) // 상 하
-                    speed %= (R - 1) * 2;
-                else if (direction == 1 || direction == 3) // 좌 우
-                    speed %= (C - 1) * 2;
-
-                for (int s = 0; s < speed; s++) {
-                    // 현재 r, c에 방향에 맞게 1칸씩 추가하며 위치 이동
-                    int ny = y + dy[direction];
-                    int nx = x + dx[direction];
-
-                    // 이동할 새로운 위치가 범위를 벗어나 벽에 부딪히면
-                    if (ny < 1 || ny > R || nx < 1 || nx > C) {
-                        y -= dy[direction]; // 다시 값 돌려주고
-                        x -= dx[direction];
-                        direction = (direction + 2) % 4; // 방향 반대로
-                        continue;
-                    }
-
-                    // 위치 벗어나지 않을때는 새로운 위치로 이동
-                    y = ny;
-                    x = nx;
-                }
-
-                sharks.put(key, new Shark(x, y, speed, direction, size));
-
-                ArrayList<Integer> list = map.get(x);
-
-                list.add(key);
-                // key : x좌표 / value : 상어 index
-                map.put(x, list);
-            }
-
-            // 3. 동일한 위치 상어 체크
-            for(int key : map.keySet()) {
-                ArrayList<Integer> list = map.get(key);
-                ArrayList<Integer> deleteItems = new ArrayList<>();
-
-                if (list.size() < 2) continue;
-
-                for (int j = 0; j < list.size(); j++) {
-                    Shark firstItem = sharks.get(list.get(j));
-
-                    for (int k = j + 1; k < list.size(); k++) {
-                        Shark secondItem = sharks.get(list.get(k));
-
-                        if (firstItem.y == secondItem.y) {
-                            deleteItems.add(firstItem.size > secondItem.size ? k : j);
-                        }
-                    }
-                }
-
-                Collections.sort(deleteItems, Collections.reverseOrder());
-
-                for(int deleteKey: deleteItems){
-                    list.remove(deleteKey);
-                }
-
-                map.put(key, list);
-            }
+            fishing(i);
+            swimming();
+            map = setUp();
         }
+        System.out.println(answer);
 
-        System.out.println(result);
     }
 
-    static class Shark {
-        int x, y, speed, direction, size;
-
-        Shark(int x, int y, int speed, int direction, int size) {
-            this.x = x;
-            this.y = y;
-            this.speed = speed;
-            this.direction = direction;
-            this.size = size;
+    static void fishing(int c) {
+        for (int i = 1; i <= R; i++) {
+            if (map[i][c] > 0) {
+                answer += map[i][c];
+                list.remove(new Shark(map[i][c]));
+                map[i][c] = 0;
+                return;
+            }
         }
     }
 
-    public static int stoi(String s) {
-        return Integer.parseInt(s);
+    static void swimming() {
+        for (int i = 0; i < list.size(); i++) {
+            Shark temp = list.get(i);
+            for (int j = 0; j < temp.s; j++) {
+                int nr = temp.r + deltas[temp.d][0];
+                int nc = temp.c + deltas[temp.d][1];
+                if (isIn(nr, nc)) {
+                    temp.r = nr;
+                    temp.c = nc;
+                } else {
+                    if (temp.d % 2 == 0)
+                        temp.d++;
+                    else
+                        temp.d--;
+                    j--;
+                }
+            }
+        }
+    }
+
+    static int[][] setUp() {
+        int[][] tempMap = new int[R + 1][C + 1];
+        for (int i = 0; i < list.size(); i++) {
+            Shark temp = list.get(i);
+            if (tempMap[temp.r][temp.c] == 0) {
+                tempMap[temp.r][temp.c] = temp.z;
+                continue;
+            }
+
+            i--;
+            if (tempMap[temp.r][temp.c] < temp.z) {
+                list.remove(new Shark(tempMap[temp.r][temp.c]));
+                tempMap[temp.r][temp.c] = temp.z;
+                continue;
+            }
+
+            list.remove(new Shark(temp.z));
+        }
+        return tempMap;
+    }
+
+    static boolean isIn(int r, int c) {
+        return r > 0 && r < R + 1 && c > 0 && c < C + 1;
     }
 }
---
+++ java/baekjoon/src/유형별문제/구현/num20056.java
@@ -0,0 +1,87 @@
+package 유형별문제.구현;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class num20056 {
+    static int N, M, K, res = 0;
+    static LinkedList<Integer>[][] map;
+    static HashMap<Integer, Fire> fireHashMap = new HashMap<>();
+    static HashSet<Integer> liveFires = new HashSet<>();
+    static int dx[] = {0, 1, 1, 1, 0, -1, -1, -1};
+    static int dy[] = {-1, -1, 0, 1, 1, 1, 0, -1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] NMK = br.readLine().split(" ");
+
+        N = stoi(NMK[0]);
+        M = stoi(NMK[1]);
+        K = stoi(NMK[2]);
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                map[i][j] = new LinkedList<>();
+            }
+        }
+
+        for (int i = 1; i <= M; i++) {
+            String[] input = br.readLine().split(" ");
+
+            int x = stoi(input[0]) - 1;
+            int y = stoi(input[1]) - 1;
+            int weight = stoi(input[2]);
+            int direction = stoi(input[3]);
+            int speed = stoi(input[4]);
+
+            fireHashMap.put(i, new Fire(y, y, direction, speed, weight));
+            liveFires.add(i);
+        }
+
+        for (int i = 0; i < K; i++) {
+            // 이동
+            for (int fireIdx : liveFires) {
+                Fire fire = fireHashMap.get(fireIdx);
+
+                fire.x = ;
+                map[fire.y][fire.x].add(fireIdx);
+            }
+
+            // 2번 처리
+
+
+        }
+
+        for (int fireIdx : liveFires) {
+            res += fireHashMap.get(fireIdx).weight;
+        }
+
+        System.out.println(res);
+    }
+
+    static boolean checkArea(int X, int Y) {
+        return X >= 0 && Y >= 0 && X < N && Y < N;
+    }
+
+    static class Fire {
+        int x, y, direction, speed, weight;
+
+        Fire(int x, int y, int direction, int speed, int weight) {
+            this.x = x;
+            this.y = y;
+            this.direction = direction;
+            this.speed = speed;
+            this.weight = weight;
+        }
+    }
+
+    public static int stoi(String s) {
+        return Integer.parseInt(s);
+    }
+}
